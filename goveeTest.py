728e50f5eb3b5c3d747adaca42083b94bd34c0fc
ChristinaCarvalho <156834326+ChristinaCarvalho@users.noreply.github.com>
2025-03-03 23:00:17 -0500

    File for Smart Home Feature
    This directly controls the power, color, white temperature, and brightness of my Govee lamp as a demonstration of smart home compatibility. Next step is to make these features controllable from the smart mirror "smart home" widget. May need to clean up code. Possible plans to implement a user input feature to switch between multiple devices on an account.

--- goveeTest.py
+++ goveeTest.py
@@ -50,26 +50,26 @@ def changeLight(type, instance, value):
 def main():
     ### Turns light on and off
     ### 0 = off, 1 = on
-    changeLight("devices.capabilities.on_off", "powerSwitch", 0)    #Off
-    changeLight("devices.capabilities.on_off", "powerSwitch", 1)    #On
+    # changeLight("devices.capabilities.on_off", "powerSwitch", 0)    #Off
+    # changeLight("devices.capabilities.on_off", "powerSwitch", 1)    #On
 
     ### changes the color of the light
     ### set r, g, b values 0-255 in colorConversion(r, g, b)
-    changeLight("devices.capabilities.color_setting", "colorRgb", int(colorConversion(255, 0, 0)))    #Red
-    changeLight("devices.capabilities.color_setting", "colorRgb", int(colorConversion(0, 255, 0)))    #Green
-    changeLight("devices.capabilities.color_setting", "colorRgb", int(colorConversion(0, 0, 255)))    #Blue
+    # changeLight("devices.capabilities.color_setting", "colorRgb", int(colorConversion(255, 0, 0)))    #Red
+    # changeLight("devices.capabilities.color_setting", "colorRgb", int(colorConversion(0, 255, 0)))    #Green
+    # changeLight("devices.capabilities.color_setting", "colorRgb", int(colorConversion(0, 0, 255)))    #Blue
 
     ### Changes the white temperature of the light
     ### My lamp uses temps between 2000k - 9000k, different models vary with their color temperature
-    changeLight("devices.capabilities.color_setting", "colorTemperatureK", 2000)    #2000k - Warm white
-    changeLight("devices.capabilities.color_setting", "colorTemperatureK", 5500)    #5500k - Pure white
-    changeLight("devices.capabilities.color_setting", "colorTemperatureK", 9000)    #9000K - Cool white
+    # changeLight("devices.capabilities.color_setting", "colorTemperatureK", 2000)    #2000k - Warm white
+    # changeLight("devices.capabilities.color_setting", "colorTemperatureK", 5500)    #5500k - Pure white
+    # changeLight("devices.capabilities.color_setting", "colorTemperatureK", 9000)    #9000K - Cool white
 
     ### Changes the brightness
     ### percentage ranges from 0-100% brightness
-    changeLight("devices.capabilities.range", "brightness", 10)    #10% brightness
-    changeLight("devices.capabilities.range", "brightness", 50)    #50% brightness
-    changeLight("devices.capabilities.range", "brightness", 100)   #100% brightness
+    # changeLight("devices.capabilities.range", "brightness", 10)    #10% brightness
+    # changeLight("devices.capabilities.range", "brightness", 50)    #50% brightness
+    # changeLight("devices.capabilities.range", "brightness", 100)   #100% brightness
 
 if __name__ == "__main__":
     main()

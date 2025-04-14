f69e9f0ffebf604806438d0b6b5292db82d616ca
Kaeden <kaedenbryer@gmail.com>
2025-04-14 15:18:50 -0400

    added code to get the agent working. I've also added my .env file (on purpose) with the API codes for Groq and Deepgram. You need to set these up to use the agent

---
+++ building_blocks/llm.py
@@ -0,0 +1,46 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+import os
+from gtts import gTTS
+from gtts.tokenizer.pre_processors import abbreviations, end_of_line
+from pygame import mixer
+import time
+
+load_dotenv()
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+with open('system_prompt.txt', 'r') as file:
+    system_prompt = file.read().strip()
+
+def batch():
+    chat = ChatGroq(temperature=0, model_name="meta-llama/llama-4-scout-17b-16e-instruct", groq_api_key=os.getenv("GROQ_API_KEY"))
+
+    system = system_prompt
+    human = "{text}"
+    prompt = ChatPromptTemplate.from_messages([("system", system), ("human", human)])
+
+    chain = prompt | chat
+    text = (chain.invoke({"text": "Explain the importance of low latency LLMs."}))
+    print(text.content)
+
+
+    tts = gTTS(text=text.content, lang='en', slow=False, pre_processor_funcs=[abbreviations, end_of_line])
+
+    tts.save("output.mp3")
+
+    mixer.init()
+    mixer.music.load("output.mp3")
+    mixer.music.play()
+    time.sleep(10)
+
+# Streaming
+def streaming():
+    chat = ChatGroq(temperature=0, model_name="meta-llama/llama-4-scout-17b-16e-instruct",groq_api_key=os.getenv("GROQ_API_KEY"))
+    prompt = ChatPromptTemplate.from_messages([("human", "Write a very long poem about {topic}")])
+    chain = prompt | chat
+    for chunk in chain.stream({"topic": "The Moon"}):
+        print(chunk.content, end="", flush=True)
+
+batch()
+# streaming()
